4b0ca4a0e60cdd977da3e2f4ced0add9e2750fa3
devhawkz <skydevhawkz@gmail.com>
2023-09-28 10:31:08 +0200

    Add project files.

---
+++ Flashcards.DEVHAWKZ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Flashcards.DEVHAWKZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
