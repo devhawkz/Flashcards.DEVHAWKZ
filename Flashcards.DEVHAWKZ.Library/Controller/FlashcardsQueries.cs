9af7d6f3bdcb6561415fbf13a3126cea4de53e63
devhawkz <skydevhawkz@gmail.com>
2023-10-17 13:42:44 +0200

    commit

---
+++ Flashcards.DEVHAWKZ.Library/Controller/FlashcardsQueries.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Flashcards.DEVHAWKZ.Library.Model;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Flashcards.DEVHAWKZ.Library.Controller;
+
+internal class FlashcardsQueries: Queries
+{
+    internal List<Flashcard> ViewFlashcards()
+    { 
+        using (IDbConnection connection = new SqlConnection(ConnectionString))
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@Id", Helpers.GetStackId());
+
+            string storedProcedureName = "ViewFlashcards";
+
+            List<Flashcard> flashcards = connection.Query<Flashcard>(storedProcedureName, commandType: CommandType.StoredProcedure).ToList();
+
+            return flashcards;
+
+        }
+    }
+}
---
+++ Flashcards.DEVHAWKZ.Library/Model/Flashcards.cs
@@ -0,0 +1,9 @@
+﻿namespace Flashcards.DEVHAWKZ.Library.Model;
+
+internal class Flashcard
+{
+    internal int FlashcardId { get; set; }
+    internal string Question { get; set; }
+    internal string Answer { get; set; }
+    internal int StackId {  get; set; }
+}
